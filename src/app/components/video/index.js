5b27eb8e0926e53f04ea03b57517c4dfc6173a61
Phil Linnell <phil@ustwo.com>
2016-12-14 15:08:15 +0000

    feat: 1st iteration of homepage hero

--- src/app/components/video/index.js
+++ src/app/components/video/index.js
@@ -1,56 +1,76 @@
 import React from 'react';
 import MediaQuery from 'react-responsive';
-
 import Rimage from 'app/components/rimage';
 
 const posterURL = "/images/transparent.png";
+
 const Video = React.createClass({
+
   render() {
     const { isVideoBackground } = this.props;
+
     if(isVideoBackground) {
       return this.renderVideoBackground();
     } else {
       return this.renderVideoEmbed();
     }
   },
+
   renderVideoEmbed() {
     const { videoId } = this.props;
     let src = "https://player.vimeo.com/video/" + videoId;
-    return <div className="video">
-      <iframe 
-        src={src}
-        width="1280" 
-        height="720" 
-        frameborder="0" 
-        title="Monument Valley - Behind the Scenes" 
-        webkitallowfullscreen
-        mozallowfullscreen
-        allowfullscreen>
-      </iframe>
-    </div>
+
+    return (
+      <div className="video">
+        <iframe
+          src={src}
+          width="1280"
+          height="720"
+          frameborder="0"
+          title="Monument Valley - Behind the Scenes"
+          webkitallowfullscreen
+          mozallowfullscreen
+          allowfullscreen>
+        </iframe>
+      </div>
+    );
   },
+
+  renderTint() {
+    if (this.props.tint) {
+      return (<div className="videoBackground-tint"></div>);
+    }
+  },
+
   renderVideoBackground() {
-    return <div className="videoBackground">
-      <MediaQuery maxWidth={768}>
-        {this.renderImage()}
-      </MediaQuery>
-      <MediaQuery minWidth={769}>
-        {this.renderVideo()}
-      </MediaQuery>
-    </div>;
-  }, 
+    return (
+      <div className="videoBackground">
+        <MediaQuery maxWidth={768}>
+          {this.renderImage()}
+        </MediaQuery>
+        <MediaQuery minWidth={769}>
+          {this.renderVideo()}
+          {this.renderTint()}
+        </MediaQuery>
+      </div>
+    );
+  },
+
   renderImage() {
     const { sizes } = this.props;
-    return <Rimage sizes={sizes} />;
+    return (<Rimage sizes={sizes} />);
   },
+
   renderVideo() {
     const { src } = this.props;
     let video;
+
     if(src && src.length) {
       video = <video src={src} poster={posterURL} autoPlay loop muted />;
     } else {
       video = this.renderImage();
     }
+
     return video;
   }
 
